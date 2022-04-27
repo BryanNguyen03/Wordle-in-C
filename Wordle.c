82c6713be82af5d25306fa9ebc9a88bcfd85db5e
Bryan <bryannguyen051012@gmail.com>
2022-04-27 14:39:05 -0400

    basic imports

---
+++ Wordle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+
+int main() {
+
+    //Returning 0 will indicate that everything ran properly and no errors occurred.
+    return 0;
+}
